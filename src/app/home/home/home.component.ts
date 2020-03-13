6f6711582c190a0a5b0b5e27a888c261c96c7d4b
Lakshan Mamalgaha <35447932+lakshanmamalgaha@users.noreply.github.com>
2020-03-13 19:09:18 +0530

    forecast form

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  formTemplate = new FormGroup({
+    days: new FormControl(''),
+    batalagoda: new FormControl(''),
+    kurunagala: new FormControl(''),
+    maspota: new FormControl()
+  });
+  isSubmitted = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get formControls() {
+    return this.formTemplate.controls;
+  }
+
+  calculate = (value) => {
+    this.isSubmitted = true;
+    if(this.formTemplate.valid){
+      console.log(value)
+    }
+
   }
 
 }
